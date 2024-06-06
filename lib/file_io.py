fdb9f5e987b48a16d8cc88da58cacce6aaa7988f
lidyaghilay <lidya.ghilay@student.moringaschool.com>
2024-06-06 21:53:44 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, file):
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as text_file:
+        return text_file.write(file)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, append):
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as text_file:
+       return text_file.write(append)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode='r', encoding='utf-8') as text_file:
+        return text_file.read()
